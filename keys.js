9fa95f73163c52e28c4c3a575c0b6b8741c2dcc5
Korwynn Wagstaff <Korwynnwagstaff@gmail.com>
2017-08-22 18:03:22 -0500

    test

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'NZXaV5R11IS0iR0YvUrlvC7IE',
+  consumer_secret: '9lmdcwQvgvdrJe4VNDomhbCZFuvn9AWHxDUuX3erqbNZOvnQ6u',
+  access_token_key: '323629548-F4bsVG4L1MwFfszOj5ZoGlMG3WzaG6b7L9xAk16p',
+  access_token_secret: '	KefWAfymX6lTxSNyM059sqvb1vGheQ1TLkG0FrasgBwb7',
+}
---
+++ liri.js
@@ -0,0 +1,5 @@
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('spotify');
+var Request = require('request');
+var fs = require('fs');
